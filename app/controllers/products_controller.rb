3f893253b1439a1fc557c9ef56519e5b1cf8e3ed
Peter Jang <peter.x.jang@gmail.com>
2017-01-23 15:16:34 -0600

    Add products new and create actions using uploads.im API

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,18 @@ class ProductsController < ApplicationController
     @products = Product.all
     render 'index.html.erb'
   end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    response = Unirest.post("http://uploads.im/api?upload", parameters: {file: params[:image]}).body
+    product = Product.new(
+      name: params[:name],
+      image_url: response["data"]["img_url"]
+    )
+    product.save
+    redirect_to "/products"
+  end
 end
